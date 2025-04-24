f6f5e07a377e402b5c54b05f5717c9922ab5daa5
labuser <rajat.sahoo@in.ey.com>
2025-04-24 21:20:57 +0000

    commit

--- src/main/java/com/ey/notification_service/NotificationServiceApplication.java
+++ src/main/java/com/ey/notification_service/NotificationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.ey.notification_service;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
--- src/main/java/com/ey/notification_service/controller/NotificationController.java
+++ src/main/java/com/ey/notification_service/controller/NotificationController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("${api.base-path}")
 public class NotificationController {
 
     @Autowired
